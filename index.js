e6c7292b8b6f1ffcb079a10442086afb2bb8cca0
immanoj16 <mmaharana14@gmail.com>
2017-12-21 00:59:19 +0530

    Add: route handler with scope of google

--- index.js
+++ index.js
@@ -5,13 +5,24 @@ const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
-}, accessToken => {
-  console.log(accessToken);
-}));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
