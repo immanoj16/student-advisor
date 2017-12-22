7c988eb731450d28a93fb63f9130012e2d6b3795
immanoj16 <mmaharana14@gmail.com>
2017-12-22 10:19:37 +0530

    Add: users collection to db

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: String,
+  name: String,
+  email: String,
+  imageURL: String,
+  gender: String
+});
+
+mongoose.model('users', userSchema);
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const Schema = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +13,19 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
+      console.log(profile.id);
+      console.log(profile.displayName);
+      console.log(profile.photos[0].value);
+      console.log(profile.gender);
+      console.log(profile.emails[0].value);
+      // console.log(profile._json.image.url);
+      new Schema({
+        googleId: profile.id,
+        name: profile.displayName,
+        email: profile.emails[0].value,
+        imageURL: profile.photos[0].value,
+        gender: profile.gender
+      }).save();
     }
   )
 );
