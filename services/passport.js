3e8d4cfcac10accf5d2ae28ca39e387250cac054
immanoj16 <mmaharana14@gmail.com>
2017-12-22 10:29:58 +0530

    Add: findOne query to store unique user

--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-const Schema = mongoose.model('users');
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -19,13 +19,21 @@ passport.use(
       console.log(profile.gender);
       console.log(profile.emails[0].value);
       // console.log(profile._json.image.url);
-      new Schema({
-        googleId: profile.id,
-        name: profile.displayName,
-        email: profile.emails[0].value,
-        imageURL: profile.photos[0].value,
-        gender: profile.gender
-      }).save();
+
+      User.findOne({ googleId: profile.id })
+        .then(existingUser => {
+          if (existingUser) {
+            
+          } else {
+            new User({
+              googleId: profile.id,
+              name: profile.displayName,
+              email: profile.emails[0].value,
+              imageURL: profile.photos[0].value,
+              gender: profile.gender
+            }).save();
+          }
+        })
     }
   )
 );
